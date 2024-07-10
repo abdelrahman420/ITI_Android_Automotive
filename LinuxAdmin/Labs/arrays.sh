9942559b84b3ee40d122c1d6017ec948b5ce378f
abdelrahman mostafa <103889376+abdelrahman420@users.noreply.github.com>
2024-07-10 17:27:39 +0300

    Delete LinuxAdmin directory

--- LinuxAdmin/Labs/arrays.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-fruits=(apple banana cherry melon mango)
-
-for i in ${!fruits[@]}; do
-    echo "index is $i value is ${fruits[$i]}"
-done
--- LinuxAdmin/Tasks/Task1/Task1-Q2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir myDirectory
-cd myDirectory
-mkdir secondDirectory
-touch myNotePaper
-cp myNotePaper ~/secondDirectory
-cd secondDirectory
-mv ~/myNotePaper ~/myOldNotePaper
